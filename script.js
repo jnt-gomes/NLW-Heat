d36dcdb51d7493fcff246315a03cd12e96c47de2
Jhonatan Gomes <jonathanisen@outlook.com.br>
2022-01-04 18:00:34 -0300

    version 6

--- script.js
+++ script.js
@@ -24,10 +24,14 @@ function getGitInfo() {
     fetch(url)
     .then(response => response.json())
     .then(data => {
-        
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        userImage.src = data.avatar_url
+        userLogin.textContent = data.login
     })
 
     // console.log(url)
 }
 
-// getGitInfo()
+getGitInfo()
